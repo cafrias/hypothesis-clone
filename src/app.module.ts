ba8473b97e88e180a4eb3b998702be1ad03abfab
Carlos Frias <carlos.a.frias@gmail.com>
2025-05-28 19:45:15 -0300

    chore: setup mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MONGO_URI } from './constants';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(MONGO_URI)],
   controllers: [AppController],
   providers: [AppService],
 })
